98778a5b361a915ea5cf507f897c487d28fd58aa
WeebNetsu <stephenvdw7777@gmail.com>
2021-06-09 14:09:08 +0200

    Deploying & Finished Result

---
+++ meteor/simple-todo-tut/8. Finish/imports/ui/TaskForm.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+function TaskForm() {
+    const [text, setText] = useState("");
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        if (text.trim() === ""){
+            return;
+        }
+
+        Meteor.call("tasks.insert", text)
+
+        setText("");
+    }
+
+    return (
+        <form className="task-form" onSubmit={handleSubmit}>
+            <input type="text" placeholder="Enter new task here" value={text} onChange={e => setText(e.target.value)} />
+            <button type="submit">Add Task</button>
+        </form>
+    )
+}
+
+export default TaskForm;
